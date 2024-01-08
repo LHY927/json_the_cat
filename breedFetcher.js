0d478f952f41ef8c3398338ceb33d10c7ff72912
LHY927 <liuhaiyue0927@gmail.com>
2024-01-09 00:18:05 +0800

    eslint it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,16 @@
 const request = require("request");
 
-const breedName = process.argv[2];
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const fetchBreedDescription = function(breedName, callback){
-    request(URL+breedName, (err, response, body) => {
-        let description = null;
-        const data = JSON.parse(body);
-        if(data.length > 0){
-            description = data[0].description;
-        }
-        callback(err, description);
-    })
-}
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL + breedName, (err, response, body) => {
+    let description = null;
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      description = data[0].description;
+    }
+    callback(err, description);
+  });
+};
 
 module.exports = {fetchBreedDescription};
